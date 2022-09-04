fa91009dc606e668a7ddb01c17a1d4edd7cfd630
Artyom <artem.khotin@gmail.com>
2022-09-05 02:35:23 +0300

    Adding plugins to another solution

---
+++ Serialization/YAML/YAMLConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Lab1.interfaces;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace YAML;
+
+public class YAMLConverter : TraceResultSerializer
+{
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        var serializer = new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+        TextWriter textWriter = new StreamWriter(to);
+        serializer.Serialize(textWriter,traceResult);
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
